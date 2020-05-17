6490a1fdc0ce3d670a20096e3735e7b9d2d32b1d
Aditya Dengare <adengare780@rku.ac.in>
2020-05-17 18:27:56 +0530

    Initial Commit

--- eCommerce/Startup.cs
+++ eCommerce/Startup.cs
@@ -21,6 +21,11 @@ namespace eCommerce
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDistributedMemoryCache();
+            services.AddSession(options => {
+                options.IdleTimeout = TimeSpan.FromMinutes(1);//You can set Time   
+            });
+
             services.AddMvc();
         }
 
@@ -38,7 +43,7 @@ namespace eCommerce
             }
 
             app.UseStaticFiles();
-
+            app.UseSession();
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
